33b89f121555bb29bc4f102e77362635d2c2ee27
Danny <danny@Daniels-MacBook-Air.local>
2025-12-02 15:11:04 +0100

    Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-import './App.css'
+import Aside from './components/Aside'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = {
+    firstName: 'Daniel',
+    lastName: 'Olugbenga',
+    skills: [
+      'project Management', 
+      'Data Analysis', 
+      'strategic Planning', 
+      'Fullstack development',
+      'Communication Skills'
+    ],
+    objective: 'Aspiring technology professional with hands-on experience in software development, RF systems, and digital solutions. I aim to leverage my problem-solving skills, engineering mindset, and growing expertise in modern technologies to build reliable, user-focused products and contribute effectively to a forward-thinking organization.',
+
+  }
 
   return (
-    <>
-      <div className='text-red-600'>Hello</div>
-    </>
+    <div id="cv" className='font-[Nunito] text-stone-700 w-screen px-3 py-7 flex items-center'>
+      <Aside />
+      <Main user={user}/>
+    </div>
   )
 }
 
