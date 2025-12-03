c086d006f76092ed435c17a66a223900ce44f14f
Danny <danny@Daniels-MacBook-Air.local>
2025-12-03 22:46:16 +0100

    Edit style and add image

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,11 +1,14 @@
 import Contact from "./contact"
 import Education from "./Education"
 import Skills from "./Skills"
+import image from '../assets/img/image.png'
+
+const profileImage = image
 
 function Aside({user}){
   return(
-    <aside className="w-[clamp(15.63rem,33%,300px)] px-6 flex flex-col items-center gap-6">
-      <img src="" alt="profile image"  className="border w-52 h-52 rounded-full"/>
+    <aside className="w-[clamp(15.63rem,33%,300px)] px-6 flex flex-col gap-6">
+      <img src={profileImage} alt="profile image"  className="w-52 h-52 rounded-full"/>
       <Contact />
       <Education />
       <Skills />
