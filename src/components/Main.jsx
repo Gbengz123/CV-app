6690d79833ceec5551722b9469b53a518388d661
Danny <danny@Daniels-MacBook-Air.local>
2025-12-05 02:38:30 +0100

    Remove prop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,11 @@ import Experience from "./Experience"
 import References from "./References"
 
 
-function Main({user}){
+function Main(){
   return(
     <main className="px-6 border-l-3 border-neutral-400 flex-1 flex flex-col gap-5">
       <Name />
-      <Objective obj={user.objective}/>
+      <Objective />
       <Experience />
       <References />
     </main>
