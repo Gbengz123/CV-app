0db8b0b86cf798e64ed792f89362311b61e33b33
Danny <danny@Daniels-MacBook-Air.local>
2025-12-05 02:33:34 +0100

    Add prop

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,12 @@
 import { Children } from "react";
 import Button from "./Button";
 
-function Section({ children, heading, hasAdd, className}){
+function Section({ children, heading, hasAdd, className, openModal}){
 
   return(
     <section className={`group flex flex-col gap-2 px-1 hover:border-2 hover:border-dashed relative ${className}`}>
       <div className="hidden group-hover:flex gap-2.5 absolute top-1 right-1.5">
-        <Button>Edit</Button>
+        <Button onClick={openModal}>Edit</Button>
         {hasAdd && <Button>Add</Button>}
       </div>
       <h1 className="text-xl font-extrabold">{heading}</h1>
