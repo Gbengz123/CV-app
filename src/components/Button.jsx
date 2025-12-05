ac6cd28127830d953fa103e7a73aa0e5dc5e186e
Danny <danny@Daniels-MacBook-Air.local>
2025-12-05 02:37:18 +0100

    Add prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
-function Button({children}){
-  return <button className="text-xs hover:cursor-pointer dashed-bg px-1">{children}</button>
+function Button({children, onClick}){
+  return <button onClick={onClick} className="text-xs hover:cursor-pointer dashed-bg px-1">{children}</button>
 }
 
 export default Button
