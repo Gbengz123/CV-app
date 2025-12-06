afe787c62acc9e99af34b8ba1c3c412f9ccaa0a7
Danny <danny@Daniels-MacBook-Air.local>
2025-12-06 01:56:26 +0100

    Add props and edit componenets

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,23 @@
 import { Children } from "react";
 import Button from "./Button";
+import { CheckIcon } from "@heroicons/react/24/solid";
 
-function Section({ children, heading, hasAdd, className, openModal}){
-
+function Section({ 
+  children, 
+  heading, 
+  hasAdd, 
+  className, 
+  handleEdit, 
+  editState, 
+  setEditState,
+  handleAdd
+}){
   return(
     <section className={`group flex flex-col gap-2 px-1 hover:border-2 hover:border-dashed relative ${className}`}>
       <div className="hidden group-hover:flex gap-2.5 absolute top-1 right-1.5">
-        <Button onClick={openModal}>Edit</Button>
-        {hasAdd && <Button>Add</Button>}
+        {!editState &&<Button onClick={handleEdit}>Edit</Button>}
+        {editState && <button onClick={() => setEditState(false)} className="w-4 h-4 cursor-pointer"><CheckIcon/></button>}
+        {hasAdd && <Button onClick={handleAdd}>Add</Button>}
       </div>
       <h1 className="text-xl font-extrabold">{heading}</h1>
       {children}
